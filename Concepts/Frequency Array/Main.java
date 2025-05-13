5c46967302729599ce7259ddf4ada7d1a177eaeb
Tharvesh <tharveshmuhaideen@gmail.com>
2025-05-13 07:36:55 +0530

    Add mode methods

--- Concepts/Frequency Array/Main.java
+++ Concepts/Frequency Array/Main.java
@@ -7,30 +7,33 @@ public class Main {
         int size = s.nextInt();
         // -------------------------------------------------------------
         int a[] = new int[size];
-        int c[] = new int[size*100];
+        int c[] = new int[1000];
 
         for (int i = 0; i < size; i++) {
             System.out.print((i + 1) + " Element: ");
-            a[i] = s.nextInt(); 
+            a[i] = s.nextInt();
         }
 
         System.out.print("Elements are: ");
         for (int i = 0; i < size; i++) {
-            System.out.print(a[i] +", ");
+            System.out.print(a[i] + ", ");
         }
 
-        for (int i = 0; i < size; c[a[i]]++, i++);
+        for (int i = 0; i < size; c[a[i]]++, i++)
+            ;
         // -------------------------------------------------------------
         System.out.println(" ");
         System.out.println("1. Unique Elements");
         System.out.println("2. Duplicate Elements");
-        System.out.println("3. Non Repeating Elements / Assending Order");
-        System.out.println("4. Least Missing Number");
-        System.out.println("5. Kth Elements");
-        System.out.println("6. occurance Sorting");
-        System.out.println(" ");
+        System.out.println("3. Non Repeating Elements");
+        System.out.println("4. Assending Order (w/o Repeat)");
+        System.out.println("5. Desending Order (w/o Repeat)");
+        System.out.println("6. Least Missing Number");
+        System.out.println("7. Kth Elements");
+        System.out.println("8. occurance based Sorting (ASC)");
+        System.out.println("9. occurance based Sorting (ASC)");
 
-        System.out.print("Enter the operation (1-6):");
+        System.out.print("Enter the operation (1-9):");
         // ------------------------------------------------------------------
         int ch = s.nextInt();
         System.out.println(" ");
@@ -53,7 +56,7 @@ public class Main {
                 }
                 break;
             case 3:
-                System.out.println("Non Repeating Elements / Assending Order: ");
+                System.out.println("Non Repeating Elements");
                 for (int i = 0; i < 1000; i++) {
                     if (c[i] > 0) {
                         System.out.print(i + " ");
@@ -61,6 +64,23 @@ public class Main {
                 }
                 break;
             case 4:
+                System.out.println("Assending Order: ");
+                for (int i = 0; i < 1000; i++) {
+                    if (c[i] > 0) {
+                        System.out.print(i + " ");
+                    }
+                }
+                break;
+
+            case 5:
+                System.out.println("Desending Order: ");
+                for (int i = 999; i >= 0; i--) {
+                    if (c[i] != 0) {
+                        System.out.print(i + " ");
+                    }
+                }
+                break;
+            case 6:
                 System.out.print("Least Missing Number: ");
                 for (int i = 1; i < 1000; i++) {
                     if (c[i] == 0) {
@@ -69,7 +89,7 @@ public class Main {
                     }
                 }
                 break;
-            case 5:
+            case 7:
                 System.out.print("Enter the Postion: ");
                 int k = s.nextInt();
 
@@ -86,9 +106,18 @@ public class Main {
 
                 }
                 break;
-            case 6:
-                System.out.println("occurance Sorting");
+            case 8:
+                System.out.println("occurance Sorting(ASC)");
                 for (int i = 1; i < 1000; i++) {
+                    for (int og = c[i]; og > 0; k = og--)
+                        if (c[i] != 0) {
+                            System.out.print(i + " ");
+                        }
+                }
+                break;
+            case 9:
+                System.out.println("occurance Sorting (DSC)");
+                for (int i = 999; i >= 0; i--) {
                     for (int og = c[i]; og > 0; k = og--)
                         if (c[i] != 0) {
                             System.out.print(i + " ");
